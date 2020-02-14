5e011c475a6a964262d3aa3dd1a203bf2b5f3eea
amber phillips <amber.ranae2@gmail.com>
2020-02-14 08:25:46 -0600

    changes made

--- frontend/coffee.js
+++ frontend/coffee.js
@@ -1,1 +1,25 @@
+// submit form
+function logSubmit(event) {
+    log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+    event.preventDefault();
+}
+
+const form = document.getElementById('form');
+const log = document.getElementById('log');
+form.addEventListener('submit', logSubmit);
+
+/// need global variables
+
+const map;
+const service;
+
+//// render the map based on users city input
+
+function renderMap() {
+    const search = document.getElementById("search").value;
+    /////
+
+    //// need if statement here to handle error if the user does not enter in the city. 
+}
+
 
